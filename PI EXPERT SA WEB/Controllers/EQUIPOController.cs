4ba8f089011c2ad4aff4129db5a1f6659fae1b50
Estudiante <estudiante@ecci.ucr.ac.cr>
2019-10-02 17:25:16 -0600

    EquipoControler created

---
+++ PI EXPERT SA WEB/Controllers/EQUIPOController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PI_EXPERT_SA_WEB.Models;
+
+namespace PI_EXPERT_SA_WEB.Controllers
+{
+    public class EQUIPOController : Controller
+    {
+        private Gr02Proy4Entities db = new Gr02Proy4Entities();
+
+        // GET: EQUIPO
+        public ActionResult Index()
+        {
+            var rOL = db.ROL.Include(r => r.EMPLEADO).Include(r => r.PROYECTO);
+            return View(rOL.ToList());
+        }
+
+        // GET: EQUIPO/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOL);
+        }
+
+        // GET: EQUIPO/Create
+        public ActionResult Create()
+        {
+            ViewBag.cedulaPK = new SelectList(db.EMPLEADO, "cedulaPK", "nombre");
+            ViewBag.idProyectoPK = new SelectList(db.PROYECTO, "idProyectoPK", "cedulaClienteFK");
+            return View();
+        }
+
+        // POST: EQUIPO/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cedulaPK,idProyectoPK,tipoRol,numEquipo")] ROL rOL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ROL.Add(rOL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cedulaPK = new SelectList(db.EMPLEADO, "cedulaPK", "nombre", rOL.cedulaPK);
+            ViewBag.idProyectoPK = new SelectList(db.PROYECTO, "idProyectoPK", "cedulaClienteFK", rOL.idProyectoPK);
+            return View(rOL);
+        }
+
+        // GET: EQUIPO/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cedulaPK = new SelectList(db.EMPLEADO, "cedulaPK", "nombre", rOL.cedulaPK);
+            ViewBag.idProyectoPK = new SelectList(db.PROYECTO, "idProyectoPK", "cedulaClienteFK", rOL.idProyectoPK);
+            return View(rOL);
+        }
+
+        // POST: EQUIPO/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cedulaPK,idProyectoPK,tipoRol,numEquipo")] ROL rOL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rOL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cedulaPK = new SelectList(db.EMPLEADO, "cedulaPK", "nombre", rOL.cedulaPK);
+            ViewBag.idProyectoPK = new SelectList(db.PROYECTO, "idProyectoPK", "cedulaClienteFK", rOL.idProyectoPK);
+            return View(rOL);
+        }
+
+        // GET: EQUIPO/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOL);
+        }
+
+        // POST: EQUIPO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ROL rOL = db.ROL.Find(id);
+            db.ROL.Remove(rOL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
