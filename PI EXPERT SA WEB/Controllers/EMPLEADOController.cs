61b66e03af8aadef1bd6cb5eca777001993d9c76
Celeste <jenoryceleste@gmail.com>
2019-10-02 18:01:41 -0600

    branch setup

---
+++ PI EXPERT SA WEB/Controllers/EMPLEADOController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PI_EXPERT_SA_WEB.Models;
+
+namespace PI_EXPERT_SA_WEB.Controllers
+{
+    public class EMPLEADOController : Controller
+    {
+        private Gr02Proy4Entities db = new Gr02Proy4Entities();
+
+        // GET: EMPLEADO
+        public ActionResult Index()
+        {
+            return View(db.EMPLEADO.ToList());
+        }
+
+        // GET: EMPLEADO/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLEADO eMPLEADO = db.EMPLEADO.Find(id);
+            if (eMPLEADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLEADO);
+        }
+
+        // GET: EMPLEADO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLEADO/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cedulaPK,nombre,apellido1,apellido2,correo,telefono,fechaContratacion,fechaDespido,tipoUsuario,disponibilidad,provincia,canton,distrito,fechaNacimiento")] EMPLEADO eMPLEADO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLEADO.Add(eMPLEADO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLEADO);
+        }
+
+        // GET: EMPLEADO/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLEADO eMPLEADO = db.EMPLEADO.Find(id);
+            if (eMPLEADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLEADO);
+        }
+
+        // POST: EMPLEADO/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cedulaPK,nombre,apellido1,apellido2,correo,telefono,fechaContratacion,fechaDespido,tipoUsuario,disponibilidad,provincia,canton,distrito,fechaNacimiento")] EMPLEADO eMPLEADO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLEADO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLEADO);
+        }
+
+        // GET: EMPLEADO/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLEADO eMPLEADO = db.EMPLEADO.Find(id);
+            if (eMPLEADO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLEADO);
+        }
+
+        // POST: EMPLEADO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            EMPLEADO eMPLEADO = db.EMPLEADO.Find(id);
+            db.EMPLEADO.Remove(eMPLEADO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
