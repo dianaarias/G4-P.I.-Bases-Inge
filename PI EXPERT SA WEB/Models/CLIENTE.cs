13a0046b3e1174de55a56e74fa2abfc921def227
dianaarias <diana.ariasrojas@ucr.ac.cr>
2019-10-10 14:42:48 -0600

    Arreglo rápido

--- PI EXPERT SA WEB/Models/CLIENTE.cs
+++ PI EXPERT SA WEB/Models/CLIENTE.cs
@@ -11,7 +11,9 @@ namespace PI_EXPERT_SA_WEB.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel;
+
     public partial class CLIENTE
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
