ea61266d1bcdd77fb83b91658c954ab85cb4d668
derrichinho <derrick0393@gmail.com>
2019-10-23 07:44:25 -0700

    Cambios en el codigo de equipo

---
+++ PI EXPERT SA WEB/Controllers/EquiposController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PI_EXPERT_SA_WEB.Models;
+
+namespace PI_EXPERT_SA_WEB.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private Gr02Proy4Entities db = new Gr02Proy4Entities();
+        // GET: Equipos
+        public ActionResult Equipo()
+        {
+            ModeloEquipo modelo = new ModeloEquipo();
+            var empleados = db.EMPLEADO.Where(t => t.disponibilidad == true).ToList();
+            return View(empleados.ToList());
+        }
+    }
+}
---
+++ PI EXPERT SA WEB/Models/ModeloEquipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PI_EXPERT_SA_WEB.Models
+{
+    public class ModeloEquipo
+    {
+        PROYECTO proyecto { get; set; }
+        List<ROL> equipo = new List<ROL>(); 
+        List<EMPLEADO> listaEmpleados = new List<EMPLEADO>();
+    }
+}
