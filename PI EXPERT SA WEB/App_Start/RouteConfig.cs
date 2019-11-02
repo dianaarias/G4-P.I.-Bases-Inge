dbcdd4a290e95a4e8174c1c01dac6b1bee809990
derrichinho <derrick0393@gmail.com>
2019-11-02 03:04:43 -0700

    Agregando Login como pagina para iniciar el programa

--- PI EXPERT SA WEB/App_Start/RouteConfig.cs
+++ PI EXPERT SA WEB/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PI_EXPERT_SA_WEB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
