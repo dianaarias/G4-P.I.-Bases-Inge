aea65dab730f4c48013a0c2e6cce0c38e15251fc
roonike <b55522@ECCI393305W4-17>
2019-11-04 14:31:58 -0600

    Duracion estimada en horas

--- PI EXPERT SA WEB/Models/REQUERIMIENTO.cs
+++ PI EXPERT SA WEB/Models/REQUERIMIENTO.cs
@@ -43,7 +43,7 @@ namespace PI_EXPERT_SA_WEB.Models
         [RegularExpression("^[0-9]*$", ErrorMessage = "La complejidad sólo puede contener números")]
         public int complejidad { get; set; }
         [RegularExpression("^[0-9]*$", ErrorMessage = "La Duración Estimada sólo puede contener números")]
-        [Display(Name = "Duración Estimada")]
+        [Display(Name = "Duración estimada en horas")]
         public Nullable<int> duracionEstimada { get; set; }
         [Display(Name = "Cédula Desarrollador")]
         public string cedulaDesarrolladorFK { get; set; }
