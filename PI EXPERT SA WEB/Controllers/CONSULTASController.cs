9d6f1e81788930e6a3020b4ed5c897ada6d8ffb2
B70873 <B70873@ECCI393240W0104>
2019-11-22 17:05:03 -0600

    pequeños cambios controller consulta

--- PI EXPERT SA WEB/Controllers/CONSULTASController.cs
+++ PI EXPERT SA WEB/Controllers/CONSULTASController.cs
@@ -1,54 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PI_EXPERT_SA_WEB.Models;
 
 namespace PI_EXPERT_SA_WEB.Controllers
 {
     public class CONSULTASController : Controller
     {
+        private Gr02Proy4Entities db = new Gr02Proy4Entities();
+
         // GET: CONSULTAS
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult DesarrolladoresAsignadosDisponibles() {
+        //public ActionResult DesarrolladoresAsignadosDisponibles() {
 
+        //}
+
+        public ActionResult ComparacionDuracionRequerimientos()
+        {
+            ViewBag.proyectos = new SelectList(db.PROYECTO, "idProyectoPK", "nombre");
+            return View();
         }
 
-        public ActionResult ComparacionDuracionRequerimientos() {
+        //public ActionResult TotalHorasRequerimiento() {
 
-        }
+        //}
 
-        public ActionResult TotalHorasRequerimiento() {
+        //public ActionResult HistorialDesarrollador() {
 
-        }
+        //}
 
-        public ActionResult HistorialDesarrollador() {
+        //public ActionResult PeriodosDesocupacion() {
 
-        }
+        //}
 
-        public ActionResult PeriodosDesocupacion() {
+        //public ActionResult ComparacionDuracionRequerimientoComplejidad() {
 
-        }
+        //}
 
-        public ActionResult ComparacionDuracionRequerimientoComplejidad() {
+        //public ActionResult ConocimientosFrecuentes() {
 
-        }
+        //}
 
-        public ActionResult ConocimientosFrecuentes() {
+        //public ActionResult EstadoResponsablesRequerimientos() {
 
-        }
+        //}
 
-        public ActionResult EstadoResponsablesRequerimientos() {
+        //public ActionResult RequerimientosTerminadosEjecucion() {
 
-        }
-
-        public ActionResult RequerimientosTerminadosEjecucion() {
-
-        }
+        //}
 
 
 
